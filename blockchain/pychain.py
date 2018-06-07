12920671c4bff36a25f34bf1706fc23c6bf100a9
Nicholas Chan <ngchan@ucdavis.edu>
2018-06-07 16:58:42 -0700

    documentation

--- blockchain/pychain.py
+++ blockchain/pychain.py
@@ -1,9 +1,12 @@
+#Import Modules
 import pandas as pd
 import time
 import binascii
 
 class bc():
     def __init__(self):
+        """Init function: Create a dataframe object
+        """
         self.chain = pd.DataFrame({'Data':[], 'Nonce':[], 'Prev Hash':[], 'Hash':[]})
         
     def mine(self, index = -1, match = '222'):
@@ -12,36 +15,52 @@ class bc():
         :param blockchain: the blockchain dataframe
         :param index: index to mine, default to last item
         """
-        
+
+        #Initialize start timer
         start = time.time()
+
+        #Get index of last element for default input
         if (index == -1):
             index = self.chain.shape[0]
-        
+
+        #Initialize datahash
         data_hash = 0
-        
+
+        #While head of hash is not equal to match string
         while (str(data_hash)[:len(match)] != match):
+            #Increment nonce
             self.chain.loc[index, 'Nonce']+=1
-            nonce = self.chain.loc[index, 'Nonce']
-            data_guess = self.chain.loc[index,'Data'] + str(nonce) + str(self.chain.loc[index, 'Prev Hash'])
+            #Concatenate all items
+            data_guess = self.chain.loc[index,'Data'] + str(self.chain.loc[index, 'Nonce']) + str(self.chain.loc[index, 'Prev Hash'])
+            #Hash guess with crc32
             data_hash = binascii.crc32(data_guess.encode('utf-8'))
-            
+
+        #Store resulting hash    
         self.chain.loc[index,'Hash'] = data_hash
+
+        #Return time to mine
         diff = time.time() - start
         return diff
 
     def addBlock(self, data, mining = True):
-        """Add a block to the blockchain.
+        """Add a block to the blockchain and mines the block.
 
         :param blockchain: the blockchain dataframe
         :param data: data to add to the blockchain
         :param mining: whether to mine the added block or not
         """
+
+        #Find index of last dataframe element
         index = self.chain.shape[0]+1
-        
+
+        #If the dataframe is empty, initialize a genesis block
         if (index == 1):
             self.chain.loc[index] = [data, 0, 0000000000, 0]
+        #Otherwise, set the previous hash field of the block
         else:
             self.chain.loc[index] = [data, 0, self.chain.loc[index-1]['Hash'],0]
+
+        #If mining parameter set, mine the block
         if mining:
             self.mine()
 
--- blockchain/pycoin.py
+++ blockchain/pycoin.py
@@ -2,25 +2,7 @@
 
 import pychain as pc
 
-bc = pc.bc()        
-
-transaction = 'AB220;CD420;5'
-
-words = ['this', 'is', 'a', 'blockchain', 'i', 'will', 'keep', 'testing', 'more', 'data', 'can', 'never', 'hurt']
-
-for word in words:
-    bc.addBlock(word)
-
-#print(bc.blockchain)
-
-#print('Attempting rehash')
-#blockchain = rehash(blockchain)
-#print(blockchain)
-#print('Changing value')
-#blockchain.loc[4, 'Data'] = 'Malicious shit'
-#print(blockchain)
-#print('Rehashing')
-#blockchain = rehash(blockchain)
-#print(blockchain)
-
-
+class coin():
+    def __init__(self):
+        self.bc = pc.bc()
+        self.wallets = []
